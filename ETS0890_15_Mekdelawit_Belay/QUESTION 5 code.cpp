fb306891ebab100cbd630b11c157be33188b6e28
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2023-11-17 22:25:31 +0300

    Create QUESTION 5 code.cpp

---
+++ ETS0890_15_Mekdelawit_Belay/QUESTION 5 code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	int X;
+	int Y;
+	
+	cout <<" ENTER THE VALUE OF X AND Y "<<endl;
+	cin>>X>>Y;
+	int power=pow(X,Y);
+	cout <<"  RESULT IS = "<<power;
+	
+	return 0;
+}
