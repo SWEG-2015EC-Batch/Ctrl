fd67fdac49e43cdadab96c782bb378c9877bb797
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2023-12-15 23:36:50 +0300

    Update 3.1Net-pay Calculator.cpp

--- ETS0890_15_Mekdelawit_Belay/3.1Net-pay Calculator.cpp
+++ ETS0890_15_Mekdelawit_Belay/3.1Net-pay Calculator.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	float grosssalary,workinghour,taxrate,bonusrateperhour,overtime,netsalary,pension,tax;
+	cout<<"enter gross salary \n";
+	cin>>grosssalary;
+	cout<<" enter working hour \n";
+	cin>>workinghour;
+		//cout<<" enter tax rate \n ";
+	    //cin>>taxrate;
+	cout<<"enter bonus rate per hour \n";
+	cin>>bonusrateperhour;
+	 if(grosssalary<=200){
+		taxrate=0;
+	}else if (grosssalary>200&&grosssalary<=600){
+	 taxrate=0.1;
+    }else if (grosssalary>600&&grosssalary<=1200){
+      taxrate=0.15;
+	}else if (grosssalary>1200&&grosssalary<=2000){
+	  taxrate=0.2;
+	}else if (grosssalary>2000&&grosssalary<=3500){
+		taxrate=0.25;
+	}else{
+		taxrate=0.3;
+	}
+      tax=grosssalary*taxrate;
+	  pension=grosssalary*(7/100);
+	  cout<< "this is your tax \n"<<tax<<endl;
+	  
+	if(workinghour>40){
+		overtime=bonusrateperhour*(workinghour-40);
+	}
+	cout<<"this is your bonus \n "<<overtime<<endl;
+	
+	netsalary=grosssalary-pension-tax+overtime;
+	cout<<"this is your net payment \n "<<netsalary<<endl;
+	
+	return 0;
+}
