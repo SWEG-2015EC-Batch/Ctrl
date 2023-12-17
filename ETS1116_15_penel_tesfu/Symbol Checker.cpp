11ccfd115e42a46b46c9c0a4071543b285830189
PenelT <149415943+PenelT@users.noreply.github.com>
2023-12-17 04:47:44 -0800

    Update Symbol Checker.cpp

--- ETS1116_15_penel_tesfu/Symbol Checker.cpp
+++ ETS1116_15_penel_tesfu/Symbol Checker.cpp
@@ -1,1 +1,54 @@
-
+#include<iostream>
+using namespace std;
+int main (){
+	  
+	  char ch;
+	  while (true){
+      
+	  cout<<"enter the character"<<endl;
+	  cin>>ch;
+	  if(((ch>='A')&&(ch<='Z'))||((ch>='a')&&(ch<='z'))){
+	  	if ((ch>='a')&&(ch<'z')){
+	  		cout<<" its lower case alphabet \n";
+	  }else{
+	  	cout<<" its uper case alphabet \n ";
+	  }
+	  switch (ch){
+	  
+	   case 'a':
+	   case 'e':
+	   case 'i':
+	   case 'o':
+	   case 'u':
+	   case 'A':
+	   case 'E':
+	   case 'I':
+	   case 'O':
+	   case 'U':
+    	cout<<"   its vowel leter \n"<<endl;
+    	break;
+	default :
+		cout<< "  its consonant leter\n ";
+	}
+	 
+} else if((ch>='0')&&(ch<='9')){
+        	cout<<"its a digt \n";
+        	if (ch%2==0){
+        		cout <<"  its even \n";
+			}else  {
+				cout<<"  its odd number \n ";
+			}
+		}
+ else {
+	cout<< "its a special character \n";
+}
+   cout<< "  enter '#' to stop or enter any key to continue "<< endl;
+   cin>>ch;
+   if (ch=='#'){
+   	exit (0);
+   }
+}
+     
+	  
+	return 0;
+}
