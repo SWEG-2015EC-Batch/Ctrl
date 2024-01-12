ae8584b2fc6d9aef2ca5c055dac563d69ce701e3
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-13 01:42:22 +0300

    Update 1D question5.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question5.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question5.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+   	// Design a program that read total mark of a students’ and print the score level with the mark in tabular
+    // format. Score status level for score >=80 Excellent, >=60 Very Good, >= 50 Fair, >=40 poor and <40 Fail.
+
+	 int n;
+	 cout<<" HOW MANY STUDENTS YOU WANT TO ENTER?"<<endl;
+	 cin>>n;
+	 double totalmark[n];
+	 for(int i=0;i<n;i++){
+	 	cout <<" Enter the marks of student "<<i+1<<endl;
+	 	cin>>totalmark[i];
+	 }
+	 for(int i=0;i<n;i++){
+	 	 if (totalmark[i] >= 80.0) {
+        cout <<" student " <<i+1<<" Excellent\n";
+    } else if (totalmark[i] >= 60.0&&totalmark[i] <80.0) {
+        cout<<" student " <<i+1 << " Very Good\n";
+    } else if (totalmark[i] < 60.0&&totalmark[i] >= 50.0) {
+        cout <<" student " <<i+1<< " Fair\n";
+    } else if (totalmark[i] >= 40.0&&totalmark[i] < 50.0) {
+        cout <<" student " <<i+1<< " Poor\n";
+    } else {
+        std::cout<<" student " <<i+1 << " Fail\n";
+    }
+	 	
+		 }
+    return 0;
+}
