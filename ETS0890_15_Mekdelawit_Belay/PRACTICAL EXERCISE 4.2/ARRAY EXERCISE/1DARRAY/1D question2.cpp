b0fb671072b57fd03d5c2b5b81eb9dbc5329ea41
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-13 01:36:36 +0300

    Update 1D question2.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question2.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+   int num[8] ,average,total=0;
+	cout<<" ENTER 8 INTEGERS"<<endl;
+	for(int i=0; i<8; i++){
+	   cout<<" ENTER THE NUMBERS"<<endl;
+	   cin>>num[i];	
+        total+=num[i];
+	   
+	}
+	 cout<<"  The total sum....."<<total<<endl; 
+       average=total/8;
+      cout<<"  The Avarage....."<<average<<endl;
+    return 0;
+}
