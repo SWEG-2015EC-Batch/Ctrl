6c4d7e97f157acc385a4068c73be1d41a0e14d14
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-13 01:38:37 +0300

    Update 1D question3.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question3.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/1DARRAY/1D question3.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+   int n;
+	cout<<"  Enter how many first/second number you want to enter "<<endl;
+	cin>>n;
+     int first[n], second[n],sum[n];
+	for (int i=0;i<n;i++){
+		cout<<" Enter the first numbers "<<endl;
+		cin>>first[i];
+		cout<<" Enter second numbers "<<endl;
+		cin>>second[i];
+	}
+
+	for (int i=0;i<n;i++){
+		cout<<"  "<<first[i];
+    }
+      cout<<endl;
+	for (int i=0;i<n;i++){
+		cout<<"  "<<second[i];
+	}
+      cout<<endl;
+	for(int i=0;i<n;i++){
+		sum[i]=second[i]+first[i];
+		cout<<sum[i]<<"  ";
+	}
+            cout<<endl;
+    return 0;
+}
