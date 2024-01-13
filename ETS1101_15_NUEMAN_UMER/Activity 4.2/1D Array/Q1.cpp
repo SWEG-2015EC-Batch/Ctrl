11217af66700f1739826f3a2e054eef4c3170d4b
Nueman2 <149424839+Nueman2@users.noreply.github.com>
2024-01-13 09:54:50 +0300

    Update Q1.cpp

--- ETS1101_15_NUEMAN_UMER/Activity 4.2/1D Array/Q1.cpp
+++ ETS1101_15_NUEMAN_UMER/Activity 4.2/1D Array/Q1.cpp
@@ -1,1 +1,11 @@
-
+//This program initialazes a 4 by 5 array called num 
+#include <iostream>
+using namespace std;
+int main() {
+    int num[4][5] = {
+        {16, 22, 99, 4, 18},
+        {-258, 4, 101, 5, 98},
+        {105, 6, 15, 2, 45},
+        {33, 88, 72, 16, 3}};
+    return 0;
+}
