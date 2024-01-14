43f434f03458370eb221940611186b24f33cf4ed
Nueman2 <149424839+Nueman2@users.noreply.github.com>
2024-01-14 23:07:29 +0300

    Create Q7.cpp

---
+++ ETS1101_15_NUEMAN_UMER/Activity 4.2/String/Q7.cpp
@@ -0,0 +1,33 @@
+//This program accept name of persons and put them in alphabetical order
+#include <iostream>
+using namespace std;
+
+int main() {
+ int size;
+    cout<<"Enter number of names you have: ";
+    cin>>size;
+    string names[size];
+
+    for (int i = 0; i < size; i++){
+        cout << "Enter name #" << i+1 << ": ";
+        getline(cin, names[i]);
+    }
+
+    for(int i = 0; i < size -1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            string temp;
+            if(names[j][0] > names[j +1][0]){
+                temp = names[j];
+                names[j] = names[j+1];
+                names[j+1] = temp;
+            }
+        }
+    }
+
+    cout << "The names in alphabetical order:-\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << names[i] << '\n';
+    }
+
+    return 0;
