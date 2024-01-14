aff80b8538bd77556af7176eabdd40b9f5844394
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-14 11:52:37 +0300

    Update QU 10.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU 10.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU 10.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <string>
+#include <cstring>
+using namespace std;
 
+int main()
+{
+  //10. Write a program that stores a password in a character array called pass. The program then
+//ask users for the password and check whether they typed the proper password or not.
+
+    char pass[10] = "qwertyuio"; // Change this to your desired password
+    char inputPassword[10];
+
+    // Ask users for the password
+   cout << "Enter the password: ";
+   cin>>inputPassword[10];
+    // Check whether the input password is correct
+    if (pass[10]==inputPassword[10]) {
+        std::cout << "Password is correct. Access granted!\n";
+    } else {
+        std::cout << "Incorrect password. Access denied!\n";
+        cout<<" Inter the correc password1!!!"<<endl;
+    }
+    return 0;
+}
