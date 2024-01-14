4c9678adc3f94fc4c78aae8e40da303cea9c141f
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-14 13:24:16 +0300

    Update QU3.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU3.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU3.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <cctype> // For using isalpha function
+
+int main() {
+ // 3. Write a program to remove non-alphabet character from string
+    std::string inputString;
+    std::string resultString;
+
+    // Input a string
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, inputString);
+
+    // Remove non-alphabetic characters from the string
+    for (char ch : inputString) {
+        if (std::isalpha(ch)) {
+            resultString += ch; // Append alphabetic characters to the result string
+        }
+    }
+
+    // Display the result
+    std::cout << "String after removing non-alphabetic characters: " << resultString << "\n";
+
+    return 0;
+}
 
