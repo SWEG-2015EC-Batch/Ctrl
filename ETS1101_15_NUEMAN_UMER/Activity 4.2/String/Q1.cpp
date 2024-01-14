ec962d6772d05b1e959f55f30abb3e059703c17f
Nueman2 <149424839+Nueman2@users.noreply.github.com>
2024-01-14 19:40:42 +0300

    Update Q1.cpp

--- ETS1101_15_NUEMAN_UMER/Activity 4.2/String/Q1.cpp
+++ ETS1101_15_NUEMAN_UMER/Activity 4.2/String/Q1.cpp
@@ -1,1 +1,37 @@
+/*this program  reads a string and count number of vowels and consonants alphabet
+present in a string.*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    int vowels = 0, consonants = 0;
+
+    cout << "Enter a word : ";
+    getline(cin, word);
+
+    for (char ch : word) {
+        if (isalpha(ch)) {
+            ch = tolower(ch);
+            switch (ch) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vowels++;
+                    break;
+                default:
+                    consonants++;
+                    break;
+            }
+        }
+    }
+
+    cout << "Number of vowels: " << vowels << endl;
+    cout << "Number of consonants: " << consonants << endl;
+
+    return 0;
+}
 
