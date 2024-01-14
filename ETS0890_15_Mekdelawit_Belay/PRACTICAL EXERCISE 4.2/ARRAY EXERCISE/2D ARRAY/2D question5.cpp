5a108985b9f49d88381863b6381f69bdfd4634fc
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-14 13:27:14 +0300

    Update 2D question5.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/2D ARRAY/2D question5.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/ARRAY EXERCISE/2D ARRAY/2D question5.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+ // 5,Suppose indices represent people and that the value at row i, column j of a 2D array is true
+//just in case i and j are friends and false otherwise. Initialize the 2D array to represent the
+//following configuration. Then the program counts how many pairs of friends are represented
+//in the array. Note that each friendship pair appears twice in the array, so in the example
+//above below there are 6 pairs of friends. 
+  
+    bool friends[6][6] = {
+
+        {false, false, true, false, false, false},
+        {false, false, false, true, false, false},
+        {true, false, true, false, true, true},
+        {false, true, false, false, true,false},
+        {false, false, true, true, true, false},
+        {false, false, true, false, false, true}
+    };
+
+    int friendnum = 0;
+    for (int i = 0; i < 6; i++) {
+        for (int j = i + 1; j < 6; j++) {
+            if (friends[i][j]) {
+                friendnum++;
+            }
+        }
+    }
+
+    cout << "Number of paired people: " << friendnum << endl;
+
+    return 0;
+}
