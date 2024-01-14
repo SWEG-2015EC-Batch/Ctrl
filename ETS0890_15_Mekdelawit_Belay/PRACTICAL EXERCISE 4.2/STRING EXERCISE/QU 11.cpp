7899c69e8a5b7fb3866a6431307cb786bb8d8e66
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-14 11:49:51 +0300

    Update QU 11.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU 11.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU 11.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <string>
+#include <cstring>
+using namespace std;
 
+int main()
+{
+  
+//11. Write a program that asks the user for its first, middle and last name and then prints the
+//user’s initials on-screen by printing the first character of each name. 
+	    string firstName, middleName, lastName;
+    // Ask the user for their first, middle, and last name
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your middle name: ";
+    cin >> middleName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    // Print the user's initials
+    cout << "\nYour initials are: " << firstName[0] << middleName[0] << lastName[0] << "\n";
+
+    return 0;
+}
