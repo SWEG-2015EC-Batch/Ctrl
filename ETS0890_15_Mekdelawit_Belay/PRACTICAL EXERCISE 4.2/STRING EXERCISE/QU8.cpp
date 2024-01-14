b5b250ed1492447e266e5bc181dc870157d1f7af
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-14 14:40:58 +0300

    Update QU8.cpp

--- ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU8.cpp
+++ ETS0890_15_Mekdelawit_Belay/PRACTICAL EXERCISE 4.2/STRING EXERCISE/QU8.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cctype>
 
+using namespace std;
+
+int main() {
+  //8. Write an application that inputs a line of text and outputs the text twice, once in all uppercase
+  //and once in all lowercase letters.
+  string text;
+
+  cout << "Enter a line of text: ";
+  getline(cin, text);
+
+  string uppercase = text;
+  for (char& c : uppercase) {
+    c = toupper(c);
+  }
+
+  string lowercase = text;
+  for (char& c : lowercase) {
+    c = tolower(c);
+  }
+
+
+  cout << "\nUppercase: " << uppercase << endl;
+  cout << "Lowercase: " << lowercase << endl;
+
+  return 0;
+}
