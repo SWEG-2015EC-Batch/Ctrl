bd439242af88d822ed5d062dfaffef9daab9398e
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-25 21:09:29 +0300

    Update PROJECT 2.cpp

--- PROJECT FoP/PROJECT 2.cpp
+++ PROJECT FoP/PROJECT 2.cpp
@@ -7,6 +7,7 @@ int main() {
       cout << "   Enter the number of rows: ";
         cin >> n;
         cout<<endl;
+        if(n>1){
     // TO DRAW THE PATTERN BASED ON THE GIVEN ROW
           for (int i = 0; i < n; i++) {
                 for (int j = 0; j < i; j++) {
@@ -25,5 +26,29 @@ int main() {
                 cout << char('A' + j);        }
             cout << endl;
              }
-                return 0;}
+        }else{
+          // ERROR MESSAGE FOR INVALID ENTER
+           cout<<"  *******ERROR********"<<endl;
+           cout<<"  ENTER THE CORRECT NUMBER OF ROW:  ";
+           cin>>n;
+           cout<<endl;
+           for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+            cout << " ";        }
+             for (int j = 0; j < n - i; j++) {
+        cout << char('A' + j);        }
+               for (int j = n - i - 2; j >= 0; j--) {
+            cout << char('A' + j);        }
+                cout << endl;    }
+                   for (int i = n - 2; i >= 0; i--) {
+                for (int j = 0; j < i; j++) {
+        cout << " ";        }
+            for (int j = 0; j < n - i; j++) {
+            cout << char('A' + j);        }
+            for (int j = n - i - 2; j >= 0; j--) {
+                cout << char('A' + j);        }
+            cout << endl;
+        }
+         }       return 0;
 
+                }
