475dad6e12e008b7d012b4e6e222444f88307627
Mekdelawit Belay <149194163+Mekde-lawit@users.noreply.github.com>
2024-01-25 20:25:21 +0300

    Update PROJECT 2.cpp

--- PROJECT FoP/PROJECT 2.cpp
+++ PROJECT FoP/PROJECT 2.cpp
@@ -1,1 +1,29 @@
+// PATTERN
+#include <iostream>
+  using namespace std;
+int main() {
+     int n;
+     // TO ENTER THE NUMBER OF THE ROW
+      cout << "   Enter the number of rows: ";
+        cin >> n;
+        cout<<endl;
+    // TO DRAW THE PATTERN BASED ON THE GIVEN ROW
+          for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+            cout << " ";        }
+             for (int j = 0; j < n - i; j++) {
+        cout << char('A' + j);        }
+               for (int j = n - i - 2; j >= 0; j--) {
+            cout << char('A' + j);        }
+                cout << endl;    }
+                   for (int i = n - 2; i >= 0; i--) {
+                for (int j = 0; j < i; j++) {
+        cout << " ";        }
+            for (int j = 0; j < n - i; j++) {
+            cout << char('A' + j);        }
+            for (int j = n - i - 2; j >= 0; j--) {
+                cout << char('A' + j);        }
+            cout << endl;
+             }
+                return 0;}
 
